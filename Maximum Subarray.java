4a42401a17ad0655004c2a0a5d4cfb0efef957fd
Eddie-Liu-Pku <34600651+Eddie-Liu-Pku@users.noreply.github.com>
2018-03-09 22:07:02 +0800

    Update Maximum Subarray.java

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum=Integer.MIN_VALUE,temp=0;
-        
+        //注意考虑如果数组都是负的情况，不要定义sum=0
         for(int i=0;i<nums.length;i++)
         {
             if(temp<0)
